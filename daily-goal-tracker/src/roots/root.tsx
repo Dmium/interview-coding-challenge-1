97b0d7ddb61084c2bc3813687ef185ac26ca1dbe
Violet <code@violethall.dev>
2024-07-28 14:57:36 +0100

    feat: Actually display links on the home page

--- daily-goal-tracker/src/roots/root.tsx
+++ daily-goal-tracker/src/roots/root.tsx
@@ -16,7 +16,7 @@ export const Root: React.FC = () => {
       {getGoals().map((goal) => {
         return (
           <div key={goal.id!}>
-            <GoalSummary goal={goal}></GoalSummary>
+            <GoalSummary goal={goal} showLinks></GoalSummary>
           </div>
         );
       })}
