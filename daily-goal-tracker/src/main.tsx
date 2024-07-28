ff38178414a42ce743b7f744662d2ffb884afacf
Violet <code@violethall.dev>
2024-07-28 12:43:11 +0100

    feat: Initialise with basic routing

---
+++ daily-goal-tracker/src/main.tsx
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Root } from "./roots/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
