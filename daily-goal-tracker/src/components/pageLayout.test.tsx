ef521d82fce198f79b5a6ea57ea96e30edd2c23c
Violet <code@violethall.dev>
2024-07-28 13:08:58 +0100

    build: Basic test setup

---
+++ daily-goal-tracker/src/components/pageLayout.test.tsx
@@ -0,0 +1,23 @@
+import { render } from "@testing-library/react";
+import { PageLayout } from "./pageLayout";
+
+describe("pageLayout", () => {
+  it("should render a title", async () => {
+    const component = render(<PageLayout title="A title"></PageLayout>);
+
+    expect((await component.findByRole("heading")).textContent).toEqual(
+      "A title"
+    );
+  });
+  it("should render children", async () => {
+    const component = render(
+      <PageLayout title="A title">
+        <p data-testId="child">This is a child</p>
+      </PageLayout>
+    );
+
+    expect((await component.findByTestId("child")).textContent).toEqual(
+      "This is a child"
+    );
+  });
+});
